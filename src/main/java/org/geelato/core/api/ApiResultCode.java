57f9c2f7af15236694f52e8ee65926a9d3a86e3c
diablo1303 <diabloposeidon@hotmail.com>
2023-05-11 23:15:35 +0800

    修改返回值成功编码[0->20000]

--- src/main/java/org/geelato/core/api/ApiResultCode.java
+++ src/main/java/org/geelato/core/api/ApiResultCode.java
@@ -4,7 +4,7 @@ package org.geelato.core.api;
  * @author geemeta
  */
 public class ApiResultCode {
-    public static final int SUCCESS = 0;
+    public static final int SUCCESS = 20000;
     public static final int ERROR = -2;
     public static final int WARNING = -1;
 }
