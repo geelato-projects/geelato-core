369a4a304ccf9cb7794e9c3a1feca90ba5872043
geelato <geelato@126.com>
2023-09-27 23:23:25 +0800

    解决时间状态转换出错的问题

--- src/main/java/org/geelato/core/orm/DateTimeConverter.java
+++ src/main/java/org/geelato/core/orm/DateTimeConverter.java
@@ -8,6 +8,8 @@ import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.Converter;
 import org.apache.commons.beanutils.PropertyUtilsBean;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.geelato.core.util.DateUtils;
 
 import java.text.DateFormat;
@@ -20,6 +22,8 @@ import java.util.Locale;
 import java.util.Map;
 
 public class DateTimeConverter implements Converter {
+
+    private static final Log log = LogFactory.getLog(DateTimeConverter.class);
     private static final String DATE = "yyyy-MM-dd";
     private static final String DATETIME = "yyyy-MM-dd HH:mm:ss";
     private static final String TIMESTAMP = "yyyy-MM-dd HH:mm:ss.SSS";
@@ -41,8 +45,9 @@ public class DateTimeConverter implements Converter {
         if (value instanceof String) {
             String dateValue = value.toString().trim();
             int length = dateValue.length();
-            if (type.equals(java.util.Date.class)) {
-                try {
+            try {
+                if (type.equals(java.util.Date.class)) {
+
                     DateFormat formatter = null;
                     if (length <= 10) {
                         formatter = new SimpleDateFormat(DATE, new DateFormatSymbols(Locale.CHINA));
@@ -56,11 +61,22 @@ public class DateTimeConverter implements Converter {
                         formatter = new SimpleDateFormat(TIMESTAMP, new DateFormatSymbols(Locale.CHINA));
                         return formatter.parse(dateValue);
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
+
+                } else if (type.equals(java.time.LocalDate.class)) {
+                    log.info("java.time.LocalDate类型，value:" + value);
+                    return new SimpleDateFormat(DATE, new DateFormatSymbols(Locale.CHINA)).parse(dateValue);
+                } else if (type.equals(java.time.LocalDateTime.class)) {
+                    log.info("java.time.LocalDateTime类型，value:" + value);
+                    return new SimpleDateFormat(DATETIME, new DateFormatSymbols(Locale.CHINA)).parse(dateValue);
+                } else if (type.equals(java.time.LocalTime.class)) {
+                    log.info("java.time.LocalTime类型，value:" + value);
+                    return value;
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+        log.info("【！！！】未识别的日期类型，type:" + type.toString() + " value:" + value);
         return value;
     }
 
