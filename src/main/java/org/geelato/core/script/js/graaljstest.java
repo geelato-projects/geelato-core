0bba14475f4a2285b24e303f81bba6a8c1bf31bb
chengx <chengx@bingosoft.net>
2023-12-05 00:05:04 +0800

    完善打包服务。

--- src/main/java/org/geelato/core/script/js/graaljstest.java
+++ src/main/java/org/geelato/core/script/js/graaljstest.java
@@ -1,6 +1,8 @@
 package org.geelato.core.script.js;
 
+import org.geelato.core.Ctx;
 import org.geelato.core.orm.Dao;
+import org.geelato.core.script.sql.SqlScriptLexer;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Value;
@@ -10,17 +12,22 @@ import java.sql.*;
 
 public class graaljstest {
     public static void  main(String[] args){
-        Context context = Context.newBuilder("js")
-                .allowHostAccess(HostAccess.ALL)
-                //allows access to all Java classes
-                .allowHostClassLookup(className -> true)
-                .build();
-        Value value = context.eval("js",
-                    "" +
-                            "var DbOp = Java.type('org.geelato.core.script.js.DbOp');var dbop = new DbOp();" +
-                            "var HashMap = Java.type('java.util.HashMap');var map = new HashMap();" +
-                            "map.put('login_name', 'admin');" +
-                            "dbop.test(map);"); //执行类中方法并传递map参数
+        System.out.println("[Java] Hello, Java!");
+        try (Context context = Context.newBuilder("js").option("js.ecmascript-version", "2020").build()) {
+            Value jsFunction = context.eval("js", "" +
+                    "(function myJavaScriptFunction(parameter,par2) {         \n" +
+                    "    console.log('[JS] Hello, ' + parameter + '!');  \n" +
+                    "    console.log('[JS] Hello, ' + JSON.stringify(par2) + '!');  \n" +
+                    "    return parameter.toUpperCase();                 \n" +
+                    "})                                                  \n");
+
+            Value result = jsFunction.execute("111",new Ctx());
+            if (result.isString()) {
+                System.out.println("[Java] result: " + result.asString());
+            } else {
+                System.out.println("[Java] unexpected result type returned from JavaScript");
+            }
+        }
     }
 
 
