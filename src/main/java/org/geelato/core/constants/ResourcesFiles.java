e3ba5168bdaf62613d1c849bf6c76b61c94a99e7
diablo1303 <diabloposeidon@hotmail.com>
2024-01-19 15:25:19 +0800

    模型权限类型变更

--- src/main/java/org/geelato/core/constants/ResourcesFiles.java
+++ src/main/java/org/geelato/core/constants/ResourcesFiles.java
@@ -12,8 +12,16 @@ public class ResourcesFiles {
      * 字段类型选择
      */
     public static final String COLUMN_SELECT_TYPE_JSON = "gelato/web/platform/data/column_select_type.json";
-
+    /**
+     * data permisson，数据权限，模型的查看权限、自定义权限。
+     */
+    public static final String PERMISSION_DATA_DEFAULT_JSON = "gelato/web/platform/data/permission_data_default.json";
+    /**
+     * model permission，实体模型权限，模型的编辑权限
+     */
+    public static final String PERMISSION_MODEL_DEFAULT_JSON = "gelato/web/platform/data/permission_model_default.json";
+    /**
+     * column permission，实体字段权限
+     */
     public static final String PERMISSION_COLUMN_DEFAULT_JSON = "gelato/web/platform/data/permission_column_default.json";
-
-    public static final String PERMISSION_TABLE_DEFAULT_JSON = "gelato/web/platform/data/permission_table_default.json";
 }
--- src/main/java/org/geelato/core/env/EnvManager.java
+++ src/main/java/org/geelato/core/env/EnvManager.java
@@ -2,7 +2,10 @@ package org.geelato.core.env;
 
 
 import org.geelato.core.Ctx;
-import org.geelato.core.env.entity.*;
+import org.geelato.core.env.entity.Permission;
+import org.geelato.core.env.entity.SysConfig;
+import org.geelato.core.env.entity.User;
+import org.geelato.core.env.entity.UserMenu;
 import org.geelato.core.orm.Dao;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
@@ -15,15 +18,15 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class EnvManager {
 
-    private final Map<String ,SysConfig> sysConfigMap;
-    private Dao  EnvDao;
+    private final Map<String, SysConfig> sysConfigMap;
+    private Dao EnvDao;
 
     private static Lock lock = new ReentrantLock();
 
     private static EnvManager instance;
 
-    private EnvManager(){
-        sysConfigMap=new HashMap<>();
+    private EnvManager() {
+        sysConfigMap = new HashMap<>();
     }
 
     public static EnvManager singleInstance() {
@@ -34,38 +37,41 @@ public class EnvManager {
         lock.unlock();
         return instance;
     }
-    public void SetDao(Dao dao){
-        this.EnvDao=dao;
+
+    public void SetDao(Dao dao) {
+        this.EnvDao = dao;
     }
-    public  void EnvInit(){
+
+    public void EnvInit() {
         LoadSysConfig();
     }
 
     private void LoadSysConfig() {
         String sql = "select config_key as configKey,config_value as configValue,app_Id as appId,tenant_code as tenantCode from platform_sys_config where enable_status =1 and del_status =0";
-        List<SysConfig> sysConfigList = EnvDao.getJdbcTemplate().query(sql,new BeanPropertyRowMapper<>(SysConfig.class));
-        for (SysConfig config:sysConfigList) {
-            if(!sysConfigMap.containsKey(config.getConfigKey())){
-                sysConfigMap.put(config.getConfigKey(),config);
+        List<SysConfig> sysConfigList = EnvDao.getJdbcTemplate().query(sql, new BeanPropertyRowMapper<>(SysConfig.class));
+        for (SysConfig config : sysConfigList) {
+            if (!sysConfigMap.containsKey(config.getConfigKey())) {
+                sysConfigMap.put(config.getConfigKey(), config);
             }
         }
     }
 
 
-    public String getConfigValue(String configKey){
-        if(this.sysConfigMap.containsKey(configKey)){
+    public String getConfigValue(String configKey) {
+        if (this.sysConfigMap.containsKey(configKey)) {
             return sysConfigMap.get(configKey).getConfigValue();
-        }else{
+        } else {
             return "unable to find this config";
         }
     }
 
-    public Map<String ,SysConfig> getConfigMap(){
+    public Map<String, SysConfig> getConfigMap() {
         return sysConfigMap;
     }
+
     public void InitCurrentUser(String loginName) {
         String sql = "select id as userId,org_id as defaultOrgId,login_name as loginName,name as userName,bu_id as buId,dept_id as deptId from platform_user  where login_name =?";
-        User dbUser = EnvDao.getJdbcTemplate().queryForObject(sql,new BeanPropertyRowMapper<User>(User.class),new Object[]{loginName});
+        User dbUser = EnvDao.getJdbcTemplate().queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), new Object[]{loginName});
         dbUser.setMenus(StructUserMenu(dbUser.getUserId()));
         dbUser.setDataPermissions(StructDataPermission(dbUser.getUserId()));
         dbUser.setElementPermissions(StructElementPermission(dbUser.getUserId()));
@@ -74,28 +80,29 @@ public class EnvManager {
     }
 
     private List<Permission> StructDataPermission(String userId) {
-        String sql = "select t2.`object`  as entity,t2.rule as rule  from platform_role_r_permission t1 \n" +
-                "left join platform_permission t2 on t1.permission_id =t2.id \n" +
-                "left join platform_role t3 on t1.role_id =t3.id \n" +
-                "left join platform_role_r_user t4 on t4.role_id =t3.id \n" +
-                "left join platform_user t5 on t5.id =t4.user_id \n" +
-                "where  t2.type='dp' and t1.del_status=0 and t5.id =?";
-        List<Permission> dataPermissionList=EnvDao.getJdbcTemplate().query(sql,
-                new BeanPropertyRowMapper<Permission>(Permission.class),new Object[]{userId});
+        String sql = "select t2.`object` as entity,t2.rule as rule from platform_role_r_permission t1 " +
+                "left join platform_permission t2 on t1.permission_id =t2.id " +
+                "left join platform_role t3 on t1.role_id =t3.id " +
+                "left join platform_role_r_user t4 on t4.role_id =t3.id " +
+                "left join platform_user t5 on t5.id =t4.user_id " +
+                "where t1.del_status=0 and t2.del_status=0 and t3.enable_status=1 and t4.del_status=0 and t5.del_status=0 " +
+                "and t2.type='dp' and t5.id =?";
+        List<Permission> dataPermissionList = EnvDao.getJdbcTemplate().query(sql,
+                new BeanPropertyRowMapper<Permission>(Permission.class), new Object[]{userId});
         return dataPermissionList;
     }
 
     private List<Permission> StructElementPermission(String userId) {
-        List<Permission> elementPermissionList=new ArrayList<>();
+        List<Permission> elementPermissionList = new ArrayList<>();
 
         return elementPermissionList;
     }
 
 
     private List<UserMenu> StructUserMenu(String userId) {
-        List<UserMenu> userMenuList=new ArrayList<>();
+        List<UserMenu> userMenuList = new ArrayList<>();
 
-        UserMenu um1=new UserMenu();
+        UserMenu um1 = new UserMenu();
         um1.setMenuUrl("");
         userMenuList.add(um1);
 
