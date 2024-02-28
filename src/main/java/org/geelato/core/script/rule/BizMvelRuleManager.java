49bfced4f1770cfcd564b198698fd432a7c953b8
chengx <chengx@bingosoft.net>
2024-02-28 16:07:47 +0800

    删除加载platform_resource的未知逻辑

--- src/main/java/org/geelato/core/script/rule/BizMvelRuleManager.java
+++ src/main/java/org/geelato/core/script/rule/BizMvelRuleManager.java
@@ -29,16 +29,17 @@ public class BizMvelRuleManager extends AbstractManager {
      */
     @Override
     public void loadDb(String sqlId) {
-        if (StringUtils.isEmpty(sqlId)) {
-            List<Map<String, Object>> mapList = dao.queryForMapList(Resources.class, "type", "biz-rule-mvel");
-            for (Map<String, Object> map : mapList) {
-                String ruleMvel = map.get("content").toString();
-                Rules rules = MVELRuleFactory.createRulesFrom(new StringReader(ruleMvel));
-                for (Rule rule : rules) {
-                    ruleMap.put(rule.getName(), rule);
-                }
-            }
-        }
+        //暂时不知道用法
+//        if (StringUtils.isEmpty(sqlId)) {
+//            List<Map<String, Object>> mapList = dao.queryForMapList(Resources.class, "type", "biz-rule-mvel");
+//            for (Map<String, Object> map : mapList) {
+//                String ruleMvel = map.get("content").toString();
+//                Rules rules = MVELRuleFactory.createRulesFrom(new StringReader(ruleMvel));
+//                for (Rule rule : rules) {
+//                    ruleMap.put(rule.getName(), rule);
+//                }
+//            }
+//        }
     }
 
     @Override
