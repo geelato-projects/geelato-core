1917dcbcc69e77bba941a7294252f74c79a49eca
chengx <chengx@bingosoft.net>
2024-06-09 20:56:51 +0800

    完善graal执行能力

--- src/main/java/org/geelato/core/orm/DaoException.java
+++ src/main/java/org/geelato/core/orm/DaoException.java
@@ -17,25 +17,4 @@ public class DaoException extends CoreException {
         this.code = ApiResultCode.ERROR;
     }
 
-    public DaoException(String msg, int code) {
-        super(msg);
-        this.msg = msg;
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
 }
